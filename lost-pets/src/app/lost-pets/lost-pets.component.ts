f30d2b61950e68c3a71bb921a84ecf33e4ac98b3
DeepikaB432 <bansaldeepika432@gmail.com>
2025-02-12 14:45:10 -0700

    contact info in lost pets page

--- lost-pets/src/app/lost-pets/lost-pets.component.ts
+++ lost-pets/src/app/lost-pets/lost-pets.component.ts
@@ -2,31 +2,34 @@ import { Component, inject } from '@angular/core';
 import { PetsService } from '../pets.service';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
-import { NgxPaginationModule } from 'ngx-pagination';  // Import NgxPaginationModule
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-lost-pets',
-  imports: [CommonModule,NgxPaginationModule],
+  imports: [CommonModule, NgxPaginationModule],
   templateUrl: './lost-pets.component.html',
   styleUrls: ['./lost-pets.component.css']
 })
 export class LostPetsComponent {
 
   petService: PetsService = inject(PetsService);
-  route: ActivatedRoute = inject(ActivatedRoute)
+  route: ActivatedRoute = inject(ActivatedRoute);
   pets_data: any = []; 
   filteredData: any = []; 
   filterActive: string = ''; 
   petType: string = '';
-  collection = [];
   p: number = 1;  // Current page for pagination
   pageSize: number = 9; // Items per page
+  isAuthenticated: boolean = false; // Track if user is logged in
 
   ngOnInit(): void {
+    // Check if the user is logged in
+    this.isAuthenticated = this.petService.isAuthenticated(); 
+
     this.route.paramMap.subscribe(params => {
       this.petType = params.get('petType') || '';
       this.getPetsData();
-    }); 
+    });
   }
 
   getPetsData(): void {
@@ -52,6 +55,6 @@ export class LostPetsComponent {
   onPageChange(event: number): void {
     this.p = event; // Set the current page number from the event
     window.scrollTo(0, 0);  // Scrolls to the top of the page
-
   }
+  
 }
